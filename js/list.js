4cc6d0357b1d7ad8513867aa7f9fb563f42c3717
jarl <67518295+Jarlyt@users.noreply.github.com>
2022-11-24 15:21:07 +0300

    Add files via upload

---
+++ js/list.js
@@ -0,0 +1,18 @@
+const d = {
+	"list": [
+		{
+			"vids": [],
+			"name": "Crimson Planet",
+			"author": "TrueOmega and more [Wooshi999]",
+			"percentToQualify": "44",
+			"verificationVid": "https://youtu.be/euMInLjyG6k",
+			"key": 2
+		},
+		
+	],
+	"version": [
+		1,
+		0,
+		0
+	]
+};const list = d.list;const version = d.version;

af3918ea363354fcc1929fdbd533a5791fa65cfb
jarl <67518295+Jarlyt@users.noreply.github.com>
2022-11-24 15:42:22 +0300

    Update list.js

--- js/list.js
+++ js/list.js
@@ -2,11 +2,19 @@ const d = {
 	"list": [
 		{
 			"vids": [],
-			"name": "??, ??",
-			"author": "?? [??]",
-			"percentToQualify": "??",
-			"verificationVid": "??",
-			"key": 2
+			"name": "WAVE CHALLENGE II, 80458684",
+			"author": "TJarly",
+			"percentToQualify": "5",
+			"verificationVid": "none",
+			"key": 0
+		},
+		{
+			"vids": [],
+			"name": "Mechanical Showdown, 27786218",
+			"author": "Tongii [TJarly]",
+			"percentToQualify": "30",
+			"verificationVid": "none",
+			"key": 1
 		},
 		
 	],
